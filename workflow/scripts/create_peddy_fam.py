e22b67d718cfd48dee3fd5e56ed352223a880775
Padraic Corcoran <padraic.corcoran@scilifelab.uu.se>
2023-09-26 15:45:07 +0200

    fix: handle cases where the full trio is not in samples.tsv

--- workflow/scripts/create_peddy_fam.py
+++ workflow/scripts/create_peddy_fam.py
@@ -17,7 +17,7 @@ def get_ped_sex(sex):
 
 
 samples = pd.read_table(
-    snakemake.input[0], dtype=str).set_index("sample", drop=False)
+    'samples.tsv', dtype=str).set_index("sample", drop=False)
 
 fam_df = samples[['sample', 'sex', 'trioid', 'trio_member']]
 
@@ -25,7 +25,7 @@ child_df = fam_df[samples.trio_member == 'proband']
 father_df = fam_df[samples.trio_member == 'father']
 mother_df = fam_df[samples.trio_member == 'mother']
 
-with open(snakemake.output[0], 'w') as pedfile:
+with open('test.ped', 'w') as pedfile:
     phenotype = '-9'
 
     for sample in fam_df.itertuples():
@@ -36,8 +36,14 @@ with open(snakemake.output[0], 'w') as pedfile:
 
         if sample.sample in child_df['sample'].tolist():
             child_trio = sample.trioid
-            paternal_id = father_df[father_df.trioid == child_trio].iat[0, 0] + '_N'
-            maternal_id = mother_df[mother_df.trioid == child_trio].iat[0, 0] + '_N'
+            try:
+                paternal_id = father_df[father_df.trioid == child_trio].iat[0, 0] + '_N'
+            except IndexError:
+                paternal_id = '0'
+            try:
+                maternal_id = mother_df[mother_df.trioid == child_trio].iat[0, 0] + '_N'
+            except IndexError:
+                maternal_id = '0'
         else:
             paternal_id = '0'
             maternal_id = '0'
