a8f3bd8c4e6f3937177902ada966ca1320fe8b05
padraicc <padraic.corcoranscilifelab.uu.se>
2025-04-22 15:46:28 +0200

    change name for singularity cache directory

--- build/build_conda.sh
+++ build/build_conda.sh
@@ -49,7 +49,7 @@ tar -zcvf ${PIPELINE_NAME}_${TAG_OR_BRANCH}.tar.gz ${PIPELINE_NAME}_${TAG_OR_BRA
 
 # Download containers
 conda activate ${PIPELINE_NAME}_${TAG_OR_BRANCH}
-hydra-genetics prepare-environment create-singularity-files -c config/config.yaml -o apptainer_cache
+hydra-genetics prepare-environment create-singularity-files -c config/config.yaml -o singularity_cache
 
 # Download references
 for reference_config in "$@"
