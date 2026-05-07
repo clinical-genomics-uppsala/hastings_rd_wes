4b906495d4e08c4af563b3d604a49e43542a01fc
Pádraic Corcoran <padraicc@users.noreply.github.com>
2026-05-07 09:56:39 +0200

    Update build/validate_ref_files.sh
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- build/validate_ref_files.sh
+++ build/validate_ref_files.sh
@@ -3,7 +3,7 @@ PIPELINE_SHORT_NAME="wp3_te"
 
 source hastings_rd_wes_${PIPELINE_VERSION}/venv/bin/activate
 
-REF_FILES=/proj/ngi2024001/nobackup/bin/${PIPELINE_SHORT_NAME}/design_and_ref_files
+REF_FILES="${REF_FILES:-$(pwd)/design_and_ref_files}"
 
 hydra-genetics --debug references validate -c hastings_rd_wes_${PIPELINE_VERSION}/hastings_rd_wes/config/config.miarka.yaml -v hastings_rd_wes_${PIPELINE_VERSION}/hastings_rd_wes/config/references/design_files.hg38.yaml \
         -v hastings_rd_wes_${PIPELINE_VERSION}/hastings_rd_wes/config/references/references.hg38.yaml -v hastings_rd_wes_${PIPELINE_VERSION}/hastings_rd_wes/config/references/exomdepth_files.hg38.yaml \
